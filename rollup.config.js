4b2d66a4ef21846cfee9ecdcd8ed799d319ea7ec
Misszhu <2503453269@qq.com>
2025-04-06 18:21:36 +0800

    feat: 改用pnpm&完善配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 export default {
   input: 'src/index.ts', // 入口文件
@@ -18,6 +18,18 @@ export default {
       plugins: [terser()]
     }
   ],
-  plugins: [resolve(), commonjs()],
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**',
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      presets: [
+        '@babel/preset-env',
+        '@babel/preset-typescript'
+      ]
+    })
+  ],
   external: ['axios', 'spark-md5']
 };
