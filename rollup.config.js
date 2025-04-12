e03589c04bde586aee2dd48552a40e16b5268d95
Misszhu <2503453269@qq.com>
2025-04-12 21:52:24 +0800

    feat: 补充包体积分析插件

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
+import filesize from 'rollup-plugin-filesize';
 
 const isProd = process.env.NODE_ENV === 'production';
 
@@ -26,6 +27,7 @@ export default {
   plugins: [
     resolve(),
     commonjs(),
+    filesize(),
     babel({
       babelHelpers: 'bundled',
       exclude: 'node_modules/**',
